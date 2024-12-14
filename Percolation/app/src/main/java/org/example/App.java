d95da03b31ac9633509f67e0b420310299c3dcaa
Jacob <jfoulds00@gmail.com>
2024-12-14 00:07:12 -0500

    main and test implementation

---
+++ Percolation/app/src/main/java/org/example/App.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.example.Percolation;
+
+public class App {
+    public static void main(String[] args) {
+        Percolation.main(new String[]{"test"});
+    } 
+}
--- Percolation/app/src/test/java/org/example/AppTest.java
+++ Percolation/app/src/test/java/org/example/AppTest.java
@@ -7,8 +7,13 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    // @Test public void appHasAGreeting() {
+    //     App classUnderTest = new App();
+    //     assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    // }
+    @Test public void testCoordToN() {
+        Percolation perc = new Percolation(20);
+        assertEquals(1, perc.coordToN(1,1));
+        assertEquals((20*20), perc.coordToN(20,20));
     }
 }
